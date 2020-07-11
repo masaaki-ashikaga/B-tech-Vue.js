61c85d80b0ab6533d2077c04ca9e358315dd0b24
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-07-11 21:21:25 +0900

    基本情報入力画面作成

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
   <div id="app">
-    <h1>Hello World !!</h1>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <first-form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstForm from './components/FirstForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FirstForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
