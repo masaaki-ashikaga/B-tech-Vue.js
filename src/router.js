f5c4355fac1aaadcf35e8bed589e94dc9e462429
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-07-14 23:02:18 +0900

    入力内容確認画面のRoute作成

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import FirstForm from './components/FirstForm.vue';
 import SecondForm from './components/SecondForm.vue';
 import ThirdForm from './components/ThirdForm.vue';
+import ResultCheck from './components/ResultCheck.vue';
 
 Vue.use(Router);
 
@@ -12,5 +13,6 @@ export default new Router({
         {path: '/', component: FirstForm},
         {path: '/second', component: SecondForm},
         {path: '/third', component: ThirdForm},
+        {path: '/check', component: ResultCheck},
     ]
 });
